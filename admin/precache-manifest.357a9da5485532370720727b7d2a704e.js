161cad02129acec41e584633012d61193bc03d9e
DiegoPQ95 <gosuetian@hotmail.es>
2020-04-30 00:13:00 -0500

    Add files via upload

---
+++ admin/precache-manifest.357a9da5485532370720727b7d2a704e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "b6827d093a7ef0f8ca03",
+    "url": "./static/js/main.b6827d09.chunk.js"
+  },
+  {
+    "revision": "ea954fcdd8d4790e0584",
+    "url": "./static/js/1.ea954fcd.chunk.js"
+  },
+  {
+    "revision": "e2b8d45cb2cf8c1127380a3fb9c93782",
+    "url": "./index.html"
+  }
+];
--- admin/service-worker.js
+++ admin/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.bfec9738f665903c1efedf35cee0a095.js"
+  "./precache-manifest.357a9da5485532370720727b7d2a704e.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("./index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
