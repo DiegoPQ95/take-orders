aa8ba772ab3e085215c0c638beae3d3fbec8ae8e
Diego Paguay <gosuetian@hotmail.es>
2020-09-10 22:31:22 -0500

    aplicación de redux, y mejoras en Payment

---
+++ precache-manifest.491f12966dfa66e029ae010414325f3e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cbfb4580ba43a3a63b26542003c41298",
+    "url": "/index.html"
+  },
+  {
+    "revision": "387ec1bdc4466e274c9e",
+    "url": "/static/js/2.baab4143.chunk.js"
+  },
+  {
+    "revision": "2fe2eb2360a44cdec691f979d5c12ef8",
+    "url": "/static/js/2.baab4143.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d453e1c23ed847d5a222",
+    "url": "/static/js/main.419a75c4.chunk.js"
+  },
+  {
+    "revision": "7c603d918ccd18192b40",
+    "url": "/static/js/runtime-main.cbdf1b61.js"
+  }
+]);
